306692a45b32849b3fd4b0764747588854beb475
Walter <silvawalteralipio@gmail.com>
2023-06-29 16:09:18 -0300

    Add controller/view for list categories

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories =_db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
