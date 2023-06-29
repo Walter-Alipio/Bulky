e40ec83ac8991e7ba61d587477e390aacf31f2d6
Walter <silvawalteralipio@gmail.com>
2023-06-29 11:39:05 -0300

    Add: Entityframeworkcore configurations

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,15 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration["ConnectionStrings"]);
+});
+
 
 var app = builder.Build();
 
